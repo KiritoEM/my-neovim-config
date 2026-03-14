5d3c781bb8321714282c2feaf513a31a8991661a
KiritoEM <johankirito64@gmail.com>
2026-03-14 19:15:36 +0300

    add keys to neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -7,7 +7,56 @@ return {
     "nvim-telescope/telescope.nvim"
   },
   cmd = "Neogit",
+   opts = {
+    kind = 'vsplit',             
+    popup = {
+      kind = 'vsplit',          
+    },
+    integrations = {
+      diffview  = true,           
+      telescope = true,         
+    },
+  },
   keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
-  }
+      {
+        '<leader>gg',
+        function() require('neogit').open() end,
+        desc = 'Open Neogit',
+      },
+      {
+        '<leader>gb',
+        function() require('neogit').open({ 'branch' }) end,
+        desc = 'Neogit branch',
+      },
+      {
+        '<leader>gc',
+        function() require('neogit').open({ 'commit' }) end,
+        desc = 'Neogit commit',
+      },
+      {
+        '<leader>gl',
+        function() require('neogit').open({ 'log' }) end,
+        desc = 'Neogit log',
+      },
+      {
+        '<leader>gp',
+        function() require('neogit').open({ 'push' }) end,
+        desc = 'Neogit push',
+      },
+      {
+        '<leader>gL',
+        function() require('neogit').open({ 'pull' }) end,
+        desc = 'Neogit pull',
+      },
+      {
+        '<leader>gw',
+        function() require('neogit').open({ 'worktree' }) end,
+        desc = 'Neogit worktree',
+      },
+      {
+        '<leader>gR',
+        function() require('neogit').open({ 'remote' }) end,
+        desc = 'Neogit remote',
+      },
+  },
 }
